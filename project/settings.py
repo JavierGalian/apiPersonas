d0727cb169127f1890ac80898b6d31afae4078e2
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2023-05-06 23:13:07 -0300

    agregando secret ket comentario

--- project/settings.py
+++ project/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# secret key render: uQ%mwRhaX[o^0/OkvhH;Zx5XGzX`.?
+
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
